101830b9bb66665ee0a860a0e136eb8fc739c088
Işık Erhan <isik.erhan.07@gmail.com>
2021-10-11 13:43:08 +0000

    Sync LeetCode submission - Length of Last Word (java)

---
+++ problems/length_of_last_word/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        
+        int i = s.length() - 1;
+        while (s.charAt(i) == ' ')
+            i--;
+        int end = i;
+        
+        while (i >= 0 && s.charAt(i) != ' ')
+            i--;
+        return end - i;
+    }
+}
