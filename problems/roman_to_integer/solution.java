e6ae16b9c09c6a52bd55a3e2c918a33c4c9900f7
Işık Erhan <isik.erhan.07@gmail.com>
2022-12-08 22:43:57 +0000

    Sync LeetCode submission - Roman to Integer (java)

--- problems/roman_to_integer/solution.java
+++ problems/roman_to_integer/solution.java
@@ -1,26 +1,37 @@
 class Solution {
-    
-    private static final Map<Character, Integer> NUMERALS = Map.of(
-        'I', 1,
-        'V', 5,
-        'X', 10,
-        'L', 50,
-        'C', 100,
-        'D', 500,
-        'M', 1000
-    );
-    
     public int romanToInt(String s) {
+        // Define a map of Roman numeral characters to their corresponding integer values
+        Map<Character, Integer> romanMap = new HashMap<>();
+        romanMap.put('I', 1);
+        romanMap.put('V', 5);
+        romanMap.put('X', 10);
+        romanMap.put('L', 50);
+        romanMap.put('C', 100);
+        romanMap.put('D', 500);
+        romanMap.put('M', 1000);
+
+        // Initialize the result to 0
         int result = 0;
-        int len = s.length();
-        for(int i = 0; i < len; i++) {
-            char c = s.charAt(i);
-            int value = NUMERALS.get(c);
-            if (i < len - 1 && value < NUMERALS.get(s.charAt(i + 1))) {
-                value *= -1;
+
+        // Iterate over the characters in the string
+        for (int i = 0; i < s.length(); i++) {
+            // Get the current and next characters
+            char current = s.charAt(i);
+            char next = i < s.length() - 1 ? s.charAt(i + 1) : '\0';
+
+            // If the current character has a lower value than the next character, subtract its value
+            // from the result. Otherwise, add its value to the result.
+            if (current == 'I' && (next == 'V' || next == 'X')) {
+                result -= romanMap.get(current);
+            } else if (current == 'X' && (next == 'L' || next == 'C')) {
+                result -= romanMap.get(current);
+            } else if (current == 'C' && (next == 'D' || next == 'M')) {
+                result -= romanMap.get(current);
+            } else {
+                result += romanMap.get(current);
             }
-            result += value;
         }
+
         return result;
     }
 }
